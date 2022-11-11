9993adee1cfc5cf247e9005020fefa6feb3150e1
parmarayushi <you@example.comaayushiparmar812@gmail.com>
2022-11-11 11:27:56 +0530

    feat:user form with standalone compoonent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsersComponent } from './users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: UsersComponent },
+  { path: 'users', loadChildren: () => import('./users/users.module').then(m => m.UsersModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
